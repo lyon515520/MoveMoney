3cb16257c92c634f8e0a1746a3dbc73a0a2a12e2
zlroy1023 <zhangli21117@gmail.com>
2015-01-17 22:42:17 +0100

    Finished: logout(switch the page to login)

--- src/com/isep/android/movemoney/Logout_Fragment.java
+++ src/com/isep/android/movemoney/Logout_Fragment.java
@@ -1,22 +1,34 @@
 package com.isep.android.movemoney;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Logout_Fragment extends Fragment {
 	
 	View rootview;
+	
+
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		rootview = inflater.inflate(R.layout.account_layout, container, false);
+		rootview = inflater.inflate(R.layout.register, container, false);
+		//to the login page
+		Intent i = new Intent(getActivity(), LoginActivity.class);
+	    startActivity(i);
+	    //stop the current activity
+	    getActivity().finish();
+	    //alert
+	    Toast.makeText(getActivity(),
+				"Successfully Logged out",
+				Toast.LENGTH_LONG).show();
 		return rootview;
 	}
-	
 
 }
