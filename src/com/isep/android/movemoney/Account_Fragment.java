fde1a7edf5efba0fb212db3b51bd71a0292179b6
lyon515520 <lyon515520@gmail.com>
2015-01-19 00:33:13 +0100

    11th commit
    finished the V1.0 of account fragment
    - retrieve the data from Parse
    -username
    -phonenumber
    -credit

--- src/com/isep/android/movemoney/Account_Fragment.java
+++ src/com/isep/android/movemoney/Account_Fragment.java
@@ -1,5 +1,7 @@
 package com.isep.android.movemoney;
 
+import com.parse.ParseUser;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -13,11 +15,29 @@ import android.widget.TextView;
 public class Account_Fragment extends Fragment {
 	View rootview;
 	
+	TextView usernametv;
+	TextView phonenumbertv;
+	TextView credittv;
+	
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		rootview = inflater.inflate(R.layout.account_layout, container, false);
+		
+		ParseUser user = ParseUser.getCurrentUser();
+		String username = user.getString("nickname");
+		String phonenumber = user.getUsername();
+		float credit = user.getInt("credit");
+		
+		usernametv = (TextView) rootview.findViewById(R.id.account_username);
+		phonenumbertv = (TextView) rootview.findViewById(R.id.account_phonenumber);
+		credittv = (TextView) rootview.findViewById(R.id.account_credit);
+		
+		usernametv.setText(username);
+		phonenumbertv.setText(phonenumber);
+		credittv.setText(String.valueOf(credit)+" euros");
+		
 		return rootview;
 	
 	}
